aa3b79c81e33422bf33ee869e81e639c74e4a193
Andy Edwards <jedwards@fastmail.com>
2017-11-29 17:32:14 -0600

    fix(rules): fix jsx indent conflict in latest eslint

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ module.exports = {
     "react/jsx-equals-spacing": ["error", "never"],
     "react/jsx-closing-bracket-location": "error",
     "react/jsx-curly-spacing": ["error", "never"],
-    "react/jsx-indent-props": "error",
+    // eslint's builtin indent rule now supports jsx
+    "react/jsx-indent-props": 0,
     "react/jsx-key": "error",
     "react/jsx-tag-spacing": "error",
     "react/no-did-mount-set-state": 0,
